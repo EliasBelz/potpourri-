ea2311388d472cf1f72fd67202f66c70c31bbd3e
eliasbelzberg <ebelz@uw.edu>
2024-05-23 12:41:23 -0700

    added position provider

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/views/potpourri_app.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/position_provider.dart';
 
 void main() {
   // TODO inti db here
   // TODO init change notifier providers here
-  runApp(PotpourriApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => PositionProvider(),
+    child: PotpourriApp(),
+  ));
 }
