63e65eb8a3a15cfa565f6898dc77d08ea3be0189
fpazaran <138263417+fpazaran@users.noreply.github.com>
2024-05-28 12:04:23 -0700

    added ratings to building card

--- flutter_app/lib/views/building_card.dart
+++ flutter_app/lib/views/building_card.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class BuildingCard extends StatelessWidget {
   final String name;
   final VoidCallback callBack;
   final String subtitle;
+  final double rating;
   const BuildingCard(
       {required this.name,
       required this.callBack,
       required this.subtitle,
+      required this.rating,
       super.key});
 
   @override
@@ -20,7 +23,24 @@ class BuildingCard extends StatelessWidget {
             child: ListTile(
               leading: const Icon(Icons.house_outlined),
               title: Text(name),
-              subtitle: Text(subtitle),
+              subtitle: Row(children: [
+                Expanded(child: Text(subtitle)),
+                Expanded(
+                  child: RatingBar(
+                    ignoreGestures: true,
+                    initialRating: rating,
+                    minRating: 0.5,
+                    maxRating: 5,
+                    allowHalfRating: true,
+                    itemSize: 20,
+                    ratingWidget: RatingWidget(
+                        full: const Icon(Icons.star),
+                        half: const Icon(Icons.star_half),
+                        empty: const Icon(Icons.star_border)),
+                    onRatingUpdate: (newRating) {},
+                  ),
+                ),
+              ]),
             )));
   }
 }
