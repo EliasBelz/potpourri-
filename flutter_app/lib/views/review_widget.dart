86be8fb3a559a289f72a56980b4a9db0c08a214b
eliasbelzberg <ebelz@uw.edu>
2024-05-29 15:18:50 -0700

    wip

--- flutter_app/lib/views/review_widget.dart
+++ flutter_app/lib/views/review_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/review_actions/rating_action.dart';
+import 'package:flutter_app/models/review_actions/review_action.dart';
+import 'package:flutter_app/models/review_actions/text_action.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import '../models/review.dart';
 
@@ -13,7 +16,7 @@ class ReviewWidget extends StatefulWidget {
   /// Parameters:
   /// review (Review): The review to be displayed.
   /// onEdit (Function): The function to be called when the review is edited.
-  const ReviewWidget({required this.review, required this.onEdit, super.key});
+  ReviewWidget({required this.review, required this.onEdit, super.key});
 
   /// Initiates state
   @override
@@ -24,6 +27,10 @@ class ReviewWidget extends StatefulWidget {
 class _ReviewWidgetState extends State<ReviewWidget> {
   /// Controller for the text box
   late final TextEditingController myController;
+  late final List<ReviewAction> pastActions;
+  late final List<ReviewAction> futureActions;
+  late double rating;
+  late String reviewText;
 
   /// Builds the widget
   @override
@@ -33,26 +40,33 @@ class _ReviewWidgetState extends State<ReviewWidget> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 2),
-            child: RatingBar(
-              ignoreGestures: !widget.review.canEdit,
-              initialRating: widget.review.rating,
-              minRating: 0.5,
-              maxRating: 5,
-              allowHalfRating: true,
-              itemSize: 30,
-              ratingWidget: RatingWidget(
-                  full: const Icon(Icons.star),
-                  half: const Icon(Icons.star_half),
-                  empty: const Icon(Icons.star_border)),
-              onRatingUpdate: (newRating) {
-                if (widget.review.canEdit) {
-                  widget.review.rating = newRating;
-                  widget.onEdit();
-                }
-              },
-            ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: RatingBar(
+                  ignoreGestures: !widget.review.canEdit,
+                  initialRating: rating,
+                  minRating: 0.5,
+                  maxRating: 5,
+                  allowHalfRating: true,
+                  itemSize: 30,
+                  ratingWidget: RatingWidget(
+                      full: const Icon(Icons.star),
+                      half: const Icon(Icons.star_half),
+                      empty: const Icon(Icons.star_border)),
+                  onRatingUpdate: (newRating) {
+                    if (widget.review.canEdit) {
+                      futureActions.clear();
+                      pastActions.add(RatingAction(newRating));
+                      setRating();
+                      widget.onEdit();
+                    }
+                  },
+                ),
+              ),
+              if (widget.review.canEdit) ...[insertUndoRedoButtons()],
+            ],
           ),
           TextField(
             enabled: widget.review.canEdit,
@@ -61,7 +75,10 @@ class _ReviewWidgetState extends State<ReviewWidget> {
             style: const TextStyle(color: Colors.black),
             maxLines: 5,
             onChanged: (text) {
-              widget.review.review = myController.text;
+              futureActions.clear();
+              pastActions.add(TextAction(text));
+              widget.review.review = text;
+              setReviewText();
               widget.onEdit();
             },
             decoration: InputDecoration(
@@ -77,11 +94,86 @@ class _ReviewWidgetState extends State<ReviewWidget> {
     );
   }
 
+  setRating() {
+    double newRating = widget.review.rating;
+    if (widget.review.canEdit) {
+      final idx =
+          pastActions.lastIndexWhere((element) => element is RatingAction);
+      if (idx == -1) {
+        newRating = 0;
+      } else {
+        newRating = (pastActions[idx] as RatingAction).rating;
+      }
+      setState(() {
+        rating = newRating;
+      });
+      widget.review.rating = newRating;
+    }
+  }
+
+  setReviewText() {
+    String newText = widget.review.review;
+    if (widget.review.canEdit) {
+      final idx =
+          pastActions.lastIndexWhere((element) => element is TextAction);
+      if (idx == -1) {
+        newText = "";
+      } else {
+        newText = (pastActions[idx] as TextAction).text;
+      }
+      setState(() {
+        reviewText = newText;
+        myController = TextEditingController(text: reviewText);
+      });
+    }
+  }
+
+  Widget insertUndoRedoButtons() {
+    return Row(
+      children: [
+        IconButton(
+            onPressed: pastActions.isEmpty ? null : _undo,
+            icon: Icon(Icons.undo)),
+        IconButton(
+            onPressed: futureActions.isEmpty ? null : _redo,
+            icon: Icon(Icons.redo)),
+      ],
+    );
+  }
+
+  void _undo() {
+    if (pastActions.isNotEmpty) {
+      final action = pastActions.removeLast();
+      futureActions.add(action);
+      if (action is RatingAction) {
+        setRating();
+      } else if (action is TextAction) {
+        setReviewText();
+      }
+    }
+  }
+
+  void _redo() {
+    if (futureActions.isNotEmpty) {
+      final action = futureActions.removeLast();
+      pastActions.add(action);
+      if (action is RatingAction) {
+        setRating();
+      } else if (action is TextAction) {
+        setReviewText();
+      }
+    }
+  }
+
   /// Initializes state with AssignmentEntry's current comments in the text box
   @override
   void initState() {
     super.initState();
-    myController = TextEditingController(text: widget.review.review);
+    reviewText = widget.review.review;
+    myController = TextEditingController(text: reviewText);
+    rating = widget.review.rating;
+    pastActions = [];
+    futureActions = [];
   }
 
   /// Deconstructor, disposes of the controller
